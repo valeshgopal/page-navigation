2b74159b004c3f1a15a015fd55b408f9e06aac7f
Valesh Gopal <valesh.dev@gmail.com>
2022-06-06 12:02:55 +0530

    Add HashRouter

--- src/App.js
+++ src/App.js
@@ -8,18 +8,18 @@ function App() {
   return (
     <>
       <div className='nav'>
-        <Link to='/component-navigation'>Home</Link>
-        <Link to='/component-navigation/about' style={{ marginLeft: '2rem' }}>
+        <Link to='/'>Home</Link>
+        <Link to='/about' style={{ marginLeft: '2rem' }}>
           About
         </Link>
-        <Link to='/component-navigation/contact' style={{ marginLeft: '2rem' }}>
+        <Link to='/contact' style={{ marginLeft: '2rem' }}>
           Contact
         </Link>
       </div>
       <Routes>
-        <Route path='/component-navigation' element={<Home />} />
-        <Route path='/component-navigation/about' element={<About />} />
-        <Route path='/component-navigation/contact' element={<Contact />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
         <Route
           path='*'
           element={<h1 style={{ marginLeft: '2rem' }}>Not Found</h1>}
